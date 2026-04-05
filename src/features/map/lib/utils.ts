9b2192e33f0a10d91d892a7d1ae4f1870a0a27cd
JongWoo <jinlaove17@naver.com>
2026-04-05 13:15:55 +0900

    chore: 함수 표현식으로 통일

--- src/features/map/lib/utils.ts
+++ src/features/map/lib/utils.ts
@@ -1,12 +1,14 @@
-export const toRadians = (degree: number) => (degree * Math.PI) / 180;
+export function toRadians(degree: number) {
+  return (degree * Math.PI) / 180;
+}
 
 // Haversine 공식(두 좌표 간 거리 계산)
-export const getDistance = (
+export function getDistance(
   lat1: number,
   lng1: number,
   lat2: number,
   lng2: number,
-) => {
+) {
   const earthRadius = 6_371_000; // 지구 반경(m)
   const latDiff = toRadians(lat2 - lat1);
   const lngDiff = toRadians(lng2 - lng1);
@@ -17,4 +19,4 @@ export const getDistance = (
       Math.sin(lngDiff / 2) ** 2;
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return earthRadius * c;
-};
+}
